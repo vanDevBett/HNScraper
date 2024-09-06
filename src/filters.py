a3404bac8b37be14aaf7a8b124a4ae59e326b881
JuanBetancourt <juan.k.ita@hotmail.es>
2024-09-06 18:06:28 -0500

    Adding the filters, 1 function to count words in the titles and 2 functions to filter by rhe number of words and rank by the number of comments or points as appropiate

---
+++ src/filters.py
@@ -0,0 +1,16 @@
+def count_words(title):
+    return len([word for word in title.split() if word.isalnum()])
+
+def filter_more_than_five_words(entries):
+    return sorted(
+        [entry for entry in entries if count_words(entry['title'])] > 5,
+        key=lambda x: x['comments'],
+        reverse=True
+    )
+
+def filter_five_or_less_words(entries):
+    return sorted(
+        [entry for entry in entries if count_words(entry['title']) <= 5],
+        key=lambda x: x['points'],
+        reverse=True
+    )
